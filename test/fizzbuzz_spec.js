70ada70b6dc503ec7a361392c0a7967d833b6d31
Ming <kkming761128kk@gapp.nthu.edu.tw>
2019-05-15 21:35:32 +0800

    add test case for function fizzBuzz()

---
+++ test/fizzbuzz_spec.js
@@ -0,0 +1,20 @@
+var should = chai.should()
+
+describe('test function fizzBuzz', function () {
+  it('print fizz if input number is divisible by 3', function () {
+    let result = fizzBuzz(9)
+    result.should.be.deep.equal('Fizz')
+  })
+  it('print buzz if input number is divisible by 5', function () {
+    let result = fizzBuzz(25)
+    result.should.be.deep.equal('Buzz')
+  })
+  it('print fizzbuzz if input number is divisible by both 3 and 5', function () {
+    let result = fizzBuzz(15)
+    result.should.be.deep.equal('FizzBuzz')
+  })
+  it('print buzz if input number is not divisible by 3 and 5', function () {
+    let result = fizzBuzz(14)
+    result.should.be.deep.equal(14)
+  })
+})
